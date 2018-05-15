442b36a96e17928e92006e5568a025987dc0e298
JaysenStark <838459914@qq.com>
2018-05-15 16:34:03 +0800

    update tree node

--- PokerTree/tree_node.py
+++ PokerTree/tree_node.py
@@ -1,10 +1,11 @@
+
+
 # node_type: inner(include check, chance, player), terminal call/fold
 # cp: current player 0, 1
 # street: 0, 1, 2, 3
 # bets: should be a tensor of size [2]
 # board: should be a tensor of size[board_card_count]
 class Node:
-
 	def __init__(self, street, board, cp, bets, node_type):
 		self.street = street
 		self.board = board
@@ -16,3 +17,10 @@ class Node:
 		self.actions = None
 		self.children = None
 		self.depth = 0
+
+		self.ranges_absolute = None
+		self.regrets = None
+		self.positive_regrets = None
+		self.strategy = None
+		self.cf_values = None
+		self.iter_weight_sum = None
