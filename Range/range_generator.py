bc56f72f38817e8d00e0a1f6cbcbb322dc305f09
JaysenStark <838459914@qq.com>
2018-05-16 15:23:03 +0800

    add function get_range_mask_and_ehs_list

--- Range/range_generator.py
+++ Range/range_generator.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 import numpy
-from Range.ehs import ExpectedHandStrength
 import random
 
 
+# range generator should generate sorted range, so it needs ehs
 class RangeGenerator:
 
-	def __init__(self):
+	def __init__(self, ehs):
 		self.board = None
 		self.range_mask = None  # a list 0 for impossible(conflict)
-		self.ehs = ExpectedHandStrength(file_path="../Data/EHS/")
+		self.ehs = ehs
+		self.ehs_list = None
 		self.sorted_ehs_structure = None
 		self.valid_range_width = None
 
@@ -24,8 +25,8 @@ class RangeGenerator:
 		rd = [0, None, None, 1, 2, 3][len(board)]
 		self.valid_range_width = [1326, 1176, 1128, 1081][rd]
 
-		ehs_list = self.ehs.get_possible_hand_ehs(board_cards=board, rd=rd)
-		self.range_mask = [0 if ehs == -1 else 1 for ehs in ehs_list]
+		self.ehs_list = self.ehs.get_possible_hand_ehs(board_cards=board, rd=rd)
+		self.range_mask = [0 if ehs == -1 else 1 for ehs in self.ehs_list]
 
 		assert self.valid_range_width == sum(self.range_mask)
 		assert rd is not None
@@ -34,12 +35,12 @@ class RangeGenerator:
 		# ehs structure = [(ehs, idx), ...], a list of tuple,
 		# where first element of tuple is expected hand strength
 		# second element of tuple is original index [0, 1, 2...]
-		ehs_structure = list(zip(ehs_list, range(len(ehs_list))))
+		ehs_structure = list(zip(self.ehs_list, range(len(self.ehs_list))))
 		# after sorting, the ehs would be like [(-1, idx), (-1, idx), ...]
 		self.sorted_ehs_structure = sorted(ehs_structure, key=lambda x: x[0])
 
+	# generate sorted ranges for both player of a batch size
 	def generate_ranges(self, batch_size):
-
 		# [1.0] generate a batch of ranges
 		ranges = numpy.ndarray((batch_size, self.valid_range_width), dtype=float)
 		probs = numpy.ones((batch_size, 1))
@@ -72,3 +73,6 @@ class RangeGenerator:
 
 			self._generate_ranges_recursively(ranges, left_probs, start, half_point)
 			self._generate_ranges_recursively(ranges, right_probs, half_point, end)
+
+	def get_mask_and_ehs_list(self):
+		return self.range_mask, self.ehs_list
