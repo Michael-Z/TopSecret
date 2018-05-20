7399f88727b2dd75fd2e275a036a95870d7475c4
JaysenStark <838459914@qq.com>
2018-05-20 15:37:12 +0800

    test mask

--- Tests/test_mask.py
+++ Tests/test_mask.py
@@ -5,11 +5,8 @@ from Equity.mask import Mask
 hole_mask = Mask.get_hole_mask()
 row_sum = hole_mask.astype("int8").sum(1)
 col_sum = hole_mask.astype("int8").sum(0)
-print(row_sum)
-for s in row_sum:
-	assert s == 1225
-for s in col_sum:
-	assert s == 1225
+assert (row_sum == 1225).all()
+assert (col_sum == 1225).all()
 
 board = [0, 1, 2]
 board_mask = Mask.get_board_mask(board=board)
