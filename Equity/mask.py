35aa2214a18bf1f66edd133819bae656e25751e0
JaysenStark <838459914@qq.com>
2018-05-22 09:48:48 +0800

    add get_xx_mask_inverse

--- Equity/mask.py
+++ Equity/mask.py
@@ -6,6 +6,7 @@ from Settings.arguments import TexasHoldemArgument as Argument
 class Mask:
 	# ndarray (1326, 1326) mask out conflict hole combinations
 	hole_mask = None  # once computed. never compute again
+	hole_mask_inverse = None  # once computed. never compute again
 
 	# once hole mask is computed, store it, and it won't change anymore
 	# hole mask only handles player holes' conflict
@@ -30,6 +31,26 @@ class Mask:
 			cls.hole_mask = valid_hole_mask
 		return cls.hole_mask
 
+	@classmethod
+	def get_hole_mask_inverse(cls):
+		if cls.hole_mask_inverse is None:
+			hc, cc = Argument.hole_count, Argument.card_count
+			valid_hole_mask = np.zeros(shape=(hc, hc), dtype="bool")
+
+			# p1 hole:(s_card, b_card), p2 hole(s, b), p1, p2 doesn't share same card
+			for s_card in range(cc - 1):
+				for b_card in range(s_card + 1, cc):
+					row_index = b_card * (b_card - 1) // 2 + s_card
+
+					# find conflict col_index
+					for s in range(cc - 1):
+						for b in range(s + 1, cc):
+							if s == s_card or b == b_card or s == b_card or b == s_card:
+								col_index = b * (b - 1) // 2 + s
+								valid_hole_mask[row_index][col_index] = 1
+			cls.hole_mask_inverse = valid_hole_mask
+		return cls.hole_mask_inverse
+
 	# return [1326] one dim ndarray shape is (1326, )
 	# @param board should be a list
 	@classmethod
@@ -42,4 +63,16 @@ class Mask:
 				if s_card in s or b_card in s:
 					index = b_card * (b_card - 1) // 2 + s_card
 					out[index] = 0
+		return out
+
+	@classmethod
+	def get_board_mask_inverse(cls, board):
+		s = set(board)
+		hc, cc = Argument.hole_count, Argument.card_count
+		out = np.zeros(shape=(hc,), dtype="bool")
+		for s_card in range(cc - 1):
+			for b_card in range(s_card + 1, cc):
+				if s_card in s or b_card in s:
+					index = b_card * (b_card - 1) // 2 + s_card
+					out[index] = 1
 		return out
--- Tests/test_mask.py
+++ Tests/test_mask.py
@@ -3,13 +3,17 @@
 from Equity.mask import Mask
 
 hole_mask = Mask.get_hole_mask()
+hole_mask_inverse = Mask.get_hole_mask_inverse()
 row_sum = hole_mask.astype("int8").sum(1)
 col_sum = hole_mask.astype("int8").sum(0)
 assert (row_sum == 1225).all()
 assert (col_sum == 1225).all()
+assert ((hole_mask + hole_mask_inverse).astype("int8") == 1).all()
 
 board = [0, 1, 2]
 board_mask = Mask.get_board_mask(board=board)
+board_mask_inverse = Mask.get_board_mask_inverse(board=board)
+assert ((board_mask + board_mask_inverse).astype("int8") == 1).all()
 assert len(board_mask) == 1326
 assert sum(board_mask) == 1176
 
