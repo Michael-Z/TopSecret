8ea4ac9ede04082be6237af2a1e8137a345a17df
JaysenStark <838459914@qq.com>
2018-05-30 19:27:57 +0800

    convert tab to spaces

--- Bucketing/bucket_conversion.py
+++ Bucketing/bucket_conversion.py
@@ -6,29 +6,29 @@ from Bucketing.static_bucketer import StaticBucketer as Bucketer
 
 class BucketConversion:
 
-	def __init__(self):
-		self.bucketer = None
-		self.bucket_count = None
-		self._range_matrix = None
-		self._reverse_value_matrix = None
+    def __init__(self):
+        self.bucketer = None
+        self.bucket_count = None
+        self._range_matrix = None
+        self._reverse_value_matrix = None
 
-	def set_board(self, board):
-		self.bucketer = Bucketer()
-		self.bucket_count = self.bucketer.get_bucket_count()
-		self._range_matrix = torch.FloatTensor(1326, self.bucket_count).zero_()
+    def set_board(self, board):
+        self.bucketer = Bucketer()
+        self.bucket_count = self.bucketer.get_bucket_count()
+        self._range_matrix = torch.FloatTensor(1326, self.bucket_count).zero_()
 
-		buckets = self.bucketer.compute_buckets(board=board)
-		class_ids = torch.arange(0, self.bucket_count)  # [0, bucket_count)
-		class_ids = class_ids.cuda() if Arguments.gpu else class_ids.float()
-		class_ids = class_ids.view(1, self.bucket_count).expand(1326, self.bucket_count)
-		card_buckets = buckets.view(1326, 1).expand(1326, self.bucket_count)
+        buckets = self.bucketer.compute_buckets(board=board)
+        class_ids = torch.arange(0, self.bucket_count)  # [0, bucket_count)
+        class_ids = class_ids.cuda() if Arguments.gpu else class_ids.float()
+        class_ids = class_ids.view(1, self.bucket_count).expand(1326, self.bucket_count)
+        card_buckets = buckets.view(1326, 1).expand(1326, self.bucket_count)
 
-		self._range_matrix[torch.eq(class_ids, card_buckets)] = 1
-		self._reverse_value_matrix = self._range_matrix.t().clone()
+        self._range_matrix[torch.eq(class_ids, card_buckets)] = 1
+        self._reverse_value_matrix = self._range_matrix.t().clone()
 
-	def card_range_2_bucket_range(self, card_range):
-		return torch.mm(card_range, self._range_matrix)
+    def card_range_2_bucket_range(self, card_range):
+        return torch.mm(card_range, self._range_matrix)
 
-	# attention, special operation may needed
-	def bucket_value_2_card_value(self, bucket_value, card_value):
-		return torch.mm(bucket_value, self._reverse_value_matrix)
+    # attention, special operation may needed
+    def bucket_value_2_card_value(self, bucket_value, card_value):
+        return torch.mm(bucket_value, self._reverse_value_matrix)
--- Bucketing/static_bucketer.py
+++ Bucketing/static_bucketer.py
@@ -4,20 +4,20 @@ from Range.ehs import ExpectedHandStrength
 
 
 class StaticBucketer:
-	def __init__(self):
-		self.ehs = ExpectedHandStrength(file_path="../Data/EHS/")
-		self.bucket_count = 500
+    def __init__(self):
+        self.ehs = ExpectedHandStrength(file_path="../Data/EHS/")
+        self.bucket_count = 500
 
-	def compute_buckets(self, board):
-		board_count = len(board)
-		assert board_count in (0, 3, 4, 5)
+    def compute_buckets(self, board):
+        board_count = len(board)
+        assert board_count in (0, 3, 4, 5)
 
-		ehs_list = self.ehs.get_possible_hand_ehs(board_cards=board)
-		ehs_tensor = torch.FloatTensor(ehs_list)
+        ehs_list = self.ehs.get_possible_hand_ehs(board_cards=board)
+        ehs_tensor = torch.FloatTensor(ehs_list)
 
-		# use fixed interval bucketing, convert ehs to [0,1,2...499]
-		buckets = ehs_tensor.clone()
-		buckets[buckets > 0].mul_(500).floor_()
+        # use fixed interval bucketing, convert ehs to [0,1,2...499]
+        buckets = ehs_tensor.clone()
+        buckets[buckets > 0].mul_(500).floor_()
 
-	def get_bucket_count(self):
-		return self.bucket_count
+    def get_bucket_count(self):
+        return self.bucket_count
