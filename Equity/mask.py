6f97ae5696c5fdf98556a1a422cb88442b508acb
JaysenStark <838459914@qq.com>
2018-05-30 16:13:53 +0800

    update mask

--- Equity/mask.py
+++ Equity/mask.py
@@ -1,43 +1,49 @@
 # -*- coding: utf-8 -*-
 import torch
-import numpy as np
 from Settings.arguments import TexasHoldemArgument as Argument
 
 
-# all return value are bool ndarray
 class Mask:
-	#  mask out conflict hole combinations
-	hole_mask = None  # FloatTensor (1326, 1326), compute once
+    """compute masks
+    'board mask' which used to mask out hole which are conflict with board
+    'hole mask' which mask out holes which are conflict with each other
 
-	# @return FloatTensor (1326, 1326)
-	@classmethod
-	def get_hole_mask(cls):
-		if cls.hole_mask is None:
-			valid_hole_mask = np.ones(shape=(1326, 1326), dtype="bool")
+    Attributes:
+        hole_mask: store hole mask once it's computed. FloatTensor (1326, 1326)
+    """
+    hole_mask = None
 
-			# p1 hole:(s_card, b_card), p2 hole(s, b), p1, p2 doesn't share same card
-			for s_card in range(51):
-				for b_card in range(s_card + 1, 52):
-					row_index = b_card * (b_card - 1) // 2 + s_card
+    @classmethod
+    def get_hole_mask(cls):
+        """return hole mask directly if it's computed, else compute it and return"""
+        if cls.hole_mask is None:
+            valid_hole_mask = torch.ByteTensor(Argument.hole_count, Argument.hole_count).fill_(1)
 
-					# find conflict col_index
-					for s in range(51):
-						for b in range(s + 1, 52):
-							if s == s_card or b == b_card or s == b_card or b == s_card:
-								col_index = b * (b - 1) // 2 + s
-								valid_hole_mask[row_index][col_index] = False
-			cls.hole_mask = valid_hole_mask
-		return cls.hole_mask
+            # p1 hole:(s_card, b_card), p2 hole(s, b), p1, p2 doesn't share same card
+            for s_card in range(51):
+                for b_card in range(s_card + 1, 52):
+                    row_index = ((b_card * (b_card - 1)) >> 1) + s_card
 
-	# @param board should be a list
-	# @return one dim bool ndarray, shape is (1326, )
-	@classmethod
-	def get_board_mask(cls, board):
-		s = set(board)
-		out = np.ones(shape=(1326, ), dtype="bool")
-		for s_card in range(51):
-			for b_card in range(s_card + 1, 52):
-				if s_card in s or b_card in s:
-					index = b_card * (b_card - 1) // 2 + s_card
-					out[index] = False
-		return out
+                    # find conflict col_index
+                    for s in range(51):
+                        for b in range(s + 1, 52):
+                            if s == s_card or b == b_card or s == b_card or b == s_card:
+                                col_index = ((b * (b - 1)) >> 1) + s
+                                valid_hole_mask[row_index][col_index] = 0
+            cls.hole_mask = valid_hole_mask
+        return cls.hole_mask
+
+    @classmethod
+    def get_board_mask(cls, board):
+        """compute board mask using given board cards
+        :parameter board: a list of board cards
+        :return board_mask: a ByteTensor (1326, )
+        """
+        s = set(board)
+        board_mask = torch.ByteTensor(Argument.hole_count).fill_(1)
+        for s_card in range(51):
+            for b_card in range(s_card + 1, 52):
+                if s_card in s or b_card in s:
+                    index = ((b_card * (b_card - 1)) >> 1) + s_card
+                    board_mask[index] = 0
+        return board_mask
--- Tests/test_mask.py
+++ Tests/test_mask.py
@@ -3,54 +3,54 @@
 from Equity.mask import Mask
 
 hole_mask = Mask.get_hole_mask()
-row_sum = hole_mask.astype(int).sum(1)
-col_sum = hole_mask.astype(int).sum(0)
+row_sum = hole_mask.float().sum(1)
+col_sum = hole_mask.float().sum(0)
 assert (row_sum == 1225).all()
 assert (col_sum == 1225).all()
 for i in range(51):
-	for j in range(i + 1, 52):
-		index1 = j * (j - 1) // 2 + i
-		for m in range(51):
-			for n in range(m + 1, 52):
-				index2 = n * (n - 1) // 2 + m
-				if i == m or i == n or j == m or j == n:
-					assert hole_mask[index1, index2] == hole_mask[index2, index1] == False
-				else:
-					assert hole_mask[index1, index2] == hole_mask[index2, index1] == True
+    for j in range(i + 1, 52):
+        index1 = j * (j - 1) // 2 + i
+        for m in range(51):
+            for n in range(m + 1, 52):
+                index2 = n * (n - 1) // 2 + m
+                if i == m or i == n or j == m or j == n:
+                    assert hole_mask[index1, index2] == hole_mask[index2, index1] == False
+                else:
+                    assert hole_mask[index1, index2] == hole_mask[index2, index1] == True
 
 
 board = [0, 1, 2]
 board_mask = Mask.get_board_mask(board=board)
-assert len(board_mask) == 1326
-assert sum(board_mask) == 1176
+assert len(board_mask.float()) == 1326
+assert sum(board_mask.float()) == 1176
 for i in range(51):
-	for j in range(i + 1, 52):
-		index = j * (j - 1) // 2 + i
-		if i in board or j in board:
-			assert not board_mask[index]
-		else:
-			assert board_mask[index]
+    for j in range(i + 1, 52):
+        index = j * (j - 1) // 2 + i
+        if i in board or j in board:
+            assert not board_mask[index]
+        else:
+            assert board_mask[index]
 
 board = [0, 1, 2, 3]
 board_mask = Mask.get_board_mask(board=board)
-assert len(board_mask) == 1326
-assert sum(board_mask) == 1128
+assert len(board_mask.float()) == 1326
+assert sum(board_mask.float()) == 1128
 for i in range(51):
-	for j in range(i + 1, 52):
-		index = j * (j - 1) // 2 + i
-		if i in board or j in board:
-			assert not board_mask[index]
-		else:
-			assert board_mask[index]
+    for j in range(i + 1, 52):
+        index = j * (j - 1) // 2 + i
+        if i in board or j in board:
+            assert not board_mask[index]
+        else:
+            assert board_mask[index]
 
 board = [0, 1, 2, 3, 4]
 board_mask = Mask.get_board_mask(board=board)
-assert len(board_mask) == 1326
-assert sum(board_mask) == 1081
+assert len(board_mask.float()) == 1326
+assert sum(board_mask.float()) == 1081
 for i in range(51):
-	for j in range(i + 1, 52):
-		index = j * (j - 1) // 2 + i
-		if i in board or j in board:
-			assert not board_mask[index]
-		else:
-			assert board_mask[index]
+    for j in range(i + 1, 52):
+        index = j * (j - 1) // 2 + i
+        if i in board or j in board:
+            assert not board_mask[index]
+        else:
+            assert board_mask[index]
