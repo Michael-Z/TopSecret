106ace528a5b8b643a750c7906d31efb6204aa13
JaysenStark <838459914@qq.com>
2018-05-31 23:00:14 +0800

    test raw samples

--- Tests/test_raw_sample.py
+++ Tests/test_raw_sample.py
@@ -19,6 +19,14 @@ for file_path in file_paths:
     range_sum = inputs[:, :-1][impossible_mask_byte].sum()
     assert value_sum == 0 == range_sum
 
+    r1 = inputs[:, 0:1326]
+    r2 = inputs[:, 1326:2652]
+    v1 = targets[:, 0:1326]
+    v2 = targets[:, 1326:2652]
+
+    k = r1 * v1 + r2 * v2
+    assert (k.sum(1) < 0.02).all() and (k.sum(1) > -0.02).all()
+
     # pots
     pots = inputs[:, -1]
     assert (100 <= pots).all() and (pots <= 20000).all()
