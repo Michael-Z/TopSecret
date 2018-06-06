ebe2f0d36edc0d5894bab40727e488499f27810d
JaysenStark <838459914@qq.com>
2018-06-06 15:13:22 +0800

    add get_uniform_range()

--- Tools/card_tools.py
+++ Tools/card_tools.py
@@ -1,11 +1,15 @@
+from Equity.mask import Mask
+from Settings.arguments import TexasHoldemArgument as Argument
 
 
 class CardTool:
 
     @classmethod
-    # @parameter boards: previous rounds board cards, list
-    # @parameter return: current round possible boards, should be a list [board1, board2, ...]
     def get_possible_future_boards(cls, boards):
+        """compute all possible future boards
+        :param boards: a list of board card
+        :return: a list of all possible future boards
+        """
         new_boards = []
         used_card_set = set(boards)
         for card in range(52):
@@ -16,3 +20,16 @@ class CardTool:
                 new_board_list.append(card)
                 new_boards.append(new_board_list)
         return new_boards
+
+    @classmethod
+    def get_uniform_range(cls, board):
+        """construct a range vector where all possible hands have same probability
+        all impossible hands have zero probability
+        :return: a uniform range which doesn't conflict with board
+        """
+        out = Argument.Tensor(Argument.hole_count).zero_()
+        mask = Mask.get_board_mask(board)
+        out[mask] = 1
+        out.div_(out.sum())
+        assert out.sum() == 1
+        return out
