19f4eeb3b4af688a603dd518905cdefd07a969db
JaysenStark <838459914@qq.com>
2018-06-20 11:34:29 +0800

    tab to space

--- NeuralNet/net_builder.py
+++ NeuralNet/net_builder.py
@@ -3,69 +3,69 @@ from torch.legacy import nn
 
 class NetBuilder:
 
-	def __init__(self):
-		self.input_size = None
-		self.output_size = None
-		self.hidden_layer_count = None
-		self.hidden_layer_size = None
+    def __init__(self):
+        self.input_size = None
+        self.output_size = None
+        self.hidden_layer_count = None
+        self.hidden_layer_size = None
 
-	def setup_in_out(self, input_size, output_size):
-		self.input_size = input_size or 1176  # C(49, 2)
-		self.output_size = output_size or 1176
+    def setup_in_out(self, input_size, output_size):
+        self.input_size = input_size
+        self.output_size = output_size
 
-	def setup_hidden_layer(self, hidden_layer_count, hidden_layer_size):
-		self.hidden_layer_count = hidden_layer_count or 3
-		self.hidden_layer_size = hidden_layer_size or 1176 // 2
+    def setup_hidden_layer(self, hidden_layer_count, hidden_layer_size):
+        self.hidden_layer_count = hidden_layer_count or 3
+        self.hidden_layer_size = hidden_layer_size or 1176 // 2
 
-	# modify v1'=r1-0.5k and v2'=r2-0.5k, where k=r1v1+r2v2
-	# r1v1' + r2v2' = r1(v1-0.5k) + r2(v2-0.5k)
-	# = r1v1+r2v2 -0.5k(sum_r1 + sum_r2)
-	# = 0
-	def build_net(self):
-		# [1.0] first layer
-		first_layer = nn.ConcatTable()
+    # modify v1'=r1-0.5k and v2'=r2-0.5k, where k=r1v1+r2v2
+    # r1v1' + r2v2' = r1(v1-0.5k) + r2(v2-0.5k)
+    # = r1v1+r2v2 -0.5k(sum_r1 + sum_r2)
+    # = 0
+    def build_net(self):
+        # [1.0] first layer
+        first_layer = nn.ConcatTable()
 
-		# [1.1] feedforward neural net, produce v1, v2
-		feedforward = nn.Sequential()
-		feedforward.add(nn.Linear(self.input_size, self.hidden_layer_size))
-		feedforward.add(nn.PReLU())
+        # [1.1] feed forward neural net, produce v1, v2
+        feedforward = nn.Sequential()
+        feedforward.add(nn.Linear(self.input_size, self.hidden_layer_size))
+        feedforward.add(nn.PReLU())
 
-		# add hidden layers
-		for i in range(self.hidden_layer_count - 1):
-			feedforward.add(nn.Linear(self.hidden_layer_size, self.hidden_layer_size))
-			feedforward.add(nn.PReLU())
+        # add hidden layers
+        for i in range(self.hidden_layer_count - 1):
+            feedforward.add(nn.Linear(self.hidden_layer_size, self.hidden_layer_size))
+            feedforward.add(nn.PReLU())
 
-		feedforward.add(nn.Linear(self.hidden_layer_size, self.output_size))
+        feedforward.add(nn.Linear(self.hidden_layer_size, self.output_size))
 
-		# [1.2] right part, discard pot_size, produce r1, r2
-		right_part = nn.Sequential()
-		right_part.add(nn.Narrow(1, 0, self.output_size))
+        # [1.2] right part, discard pot_size, produce r1, r2
+        right_part = nn.Sequential()
+        right_part.add(nn.Narrow(1, 0, self.output_size))
 
-		first_layer.add(feedforward)
-		first_layer.add(right_part)
+        first_layer.add(feedforward)
+        first_layer.add(right_part)
 
-		# [2.0] outer net force counterfactual values satisfy 0-sum property
-		second_layer = nn.ConcatTable()
+        # [2.0] outer net force counterfactual values satisfy 0-sum property
+        second_layer = nn.ConcatTable()
 
-		# accept v1,v2; ignore r1, r2
-		left_part2 = nn.Sequential()
-		left_part2.add(nn.SelectTable(0))
+        # accept v1,v2; ignore r1, r2
+        left_part2 = nn.Sequential()
+        left_part2.add(nn.SelectTable(0))
 
-		# accept, r1,r2, v1,v2; produce -0.5k=-0.5(r1v1 + r2v2)
-		right_part2 = nn.Sequential()
-		right_part2.add(nn.DotProduct())
-		right_part2.add(nn.Unsqueeze(1))
-		right_part2.add(nn.Replicate(self.output_size, 1))
-		right_part2.add(nn.Squeeze(2))
-		right_part2.add(nn.MulConstant(-0.5))
+        # accept, r1,r2, v1,v2; produce -0.5k=-0.5(r1v1 + r2v2)
+        right_part2 = nn.Sequential()
+        right_part2.add(nn.DotProduct())
+        right_part2.add(nn.Unsqueeze(1))
+        right_part2.add(nn.Replicate(self.output_size, 1))
+        right_part2.add(nn.Squeeze(2))
+        right_part2.add(nn.MulConstant(-0.5))
 
-		second_layer.add(left_part2)
-		second_layer.add(right_part2)
+        second_layer.add(left_part2)
+        second_layer.add(right_part2)
 
-		final_mlp = nn.Sequential()
-		final_mlp.add(first_layer)
-		final_mlp.add(second_layer)
-		# accept v1,v2 and -0.5k, product v1-0.5k, v2-0.5k
-		final_mlp.add(nn.CAddTable())
+        final_mlp = nn.Sequential()
+        final_mlp.add(first_layer)
+        final_mlp.add(second_layer)
+        # accept v1,v2 and -0.5k, product v1-0.5k, v2-0.5k
+        final_mlp.add(nn.CAddTable())
 
-		return final_mlp
+        return final_mlp
--- NeuralNet/trainer.py
+++ NeuralNet/trainer.py
@@ -1,82 +1,82 @@
-from torch.legacy import optim
-from Settings.arguments import TexasHoldemAgrument
-from NeuralNet.masked_huber_loss import MaskedHuberLoss
 import time
+from torch.legacy import optim
+from NeuralNet.masked_huber_loss import MaskedHuberLoss
+from Settings.arguments import TexasHoldemArgument as Argument
 
 
 class Trainer:
 
-	def __init__(self, network, data_stream, learning_rate=None):
-		self.network = network
-		self.data_stream = data_stream
-		self.params, self.grads = self.network.flattenParameters()
-		self.criterion = MaskedHuberLoss()
-		self.learning_rate = learning_rate or TexasHoldemAgrument.learning_rate
-		self.optim_func = optim.adam
+    def __init__(self, network, data_stream, learning_rate=None):
+        self.network = network
+        self.data_stream = data_stream
+        self.params, self.grads = self.network.flattenParameters()
+        self.criterion = MaskedHuberLoss()
+        self.learning_rate = learning_rate or Argument.learning_rate
+        self.optim_func = optim.adam
 
-	def train(self, epoch_count):
-		start_time = time.time()
+    def train(self, epoch_count):
+        start_time = time.time()
 
-		for epoch in range(epoch_count):
-			self.data_stream.start_epoch()
-			loss_sum = 0
-			state = {"learningRate": self.learning_rate}
-			for i in range(self.data_stream.get_train_batch_count()):
-				inputs, targets, mask = self.data_stream.get_train_batch(i)
-				# todo learning rate
-				_, loss = self.optim_func(lambda x: self.feval(x, inputs, targets, mask), self.params, state)
-				loss_sum += loss
-			# print("training loss:%d" % loss_sum / self.data_stream.get_train_batch_count())
-			print(loss_sum)
-			print("epoch %d" % epoch)
-		end_time = time.time()
-		print("training time cost", end_time - start_time)
+        for epoch in range(epoch_count):
+            self.data_stream.start_epoch()
+            loss_sum = 0
+            state = {"learningRate": self.learning_rate}
+            for i in range(self.data_stream.get_train_batch_count()):
+                inputs, targets, mask = self.data_stream.get_train_batch(i)
+                # todo learning rate
+                _, loss = self.optim_func(lambda x: self.feval(x, inputs, targets, mask), self.params, state)
+                loss_sum += loss
 
-		valid_loss_sum = 0
-		for i in range(self.data_stream.get_valid_batch_count()):
-			inputs, targets, mask = self.data_stream.get_valid_batch(batch_index=i)
-			outputs = self.network.forward(inputs)
-			loss = self.criterion.forward(outputs, targets, mask)
-			valid_loss_sum += loss
+            print("training loss:%f" % (loss_sum / self.data_stream.get_train_batch_count()))
+            print("epoch %d" % epoch)
+        end_time = time.time()
+        print("training time cost", end_time - start_time)
 
-		valid_loss = valid_loss_sum / self.data_stream.get_valid_batch_count()
-		print("valid loss:", valid_loss)
+        valid_loss_sum = 0
+        for i in range(self.data_stream.get_valid_batch_count()):
+            inputs, targets, mask = self.data_stream.get_valid_batch(batch_index=i)
+            outputs = self.network.forward(inputs)
+            loss = self.criterion.forward(outputs, targets, mask)
+            valid_loss_sum += loss
 
-	# function pass to optim package
-	# @param params_new the neural network parameters
-	# @param inputs the neural network inputs
-	# @param targets the neural network targets
-	# @param mask the mask vector used for the loss function
-	# @return the masked huber loss on 'inputs' and 'targets'
-	# @return the gradient of the loss function
-	def feval(self, params_new, inputs, targets, mask):
-		# set x to x_new, if different
-		# in this simple implementation, it's useless, since x_new always points to x
-		if self.params is not params_new:
-			self.params.copy_(params_new)
+        valid_loss = valid_loss_sum / self.data_stream.get_valid_batch_count()
+        print("valid loss:", valid_loss)
 
-		# zero grads, compute outputs and loss
-		self.grads.zero_()
-		outputs = self.network.forward(inputs)
-		loss = self.criterion.forward(outputs, targets, mask)
+    def feval(self, params_new, inputs, targets, mask):
+        # set x to x_new, if different
+        # in this simple implementation, it's useless, since x_new always points to x
+        if self.params is not params_new:
+            self.params.copy_(params_new)
 
-		# backward
-		dloss_doutput = self.criterion.backward(outputs, targets)
-		self.network.backward(inputs, dloss_doutput)
+        # zero grads, compute outputs and loss
+        self.grads.zero_()
+        outputs = self.network.forward(inputs)
+        loss = self.criterion.forward(outputs, targets, mask)
 
-		return loss, self.grads
+        # backward
+        dloss_doutput = self.criterion.backward(outputs, targets)
+        self.network.backward(inputs, dloss_doutput)
+
+        return loss, self.grads
 
 
 def main():
-	from NeuralNet.net_builder import NetBuilder
-	from NeuralNet.data_stream import DataStream
-	nb = NetBuilder()
-	nb.setup_in_out(input_size=1176, output_size=1176)
-	nb.setup_hidden_layer(hidden_layer_count=3, hidden_layer_size=588)
-	net = nb.build_net()
-	ds = None
+    from NeuralNet.net_builder import NetBuilder
+    from NeuralNet.data_stream import DataStream
+    nb = NetBuilder()
+    nb.setup_in_out(input_size=1001, output_size=1000)
+    nb.setup_hidden_layer(hidden_layer_count=3, hidden_layer_size=500)
+    net = nb.build_net()
+
+    ds = DataStream()
+    ds.load_data()
+    ds.split_data(fraction=0.9)
+    ds.setup_batch_size(train_batch_size=100, valid_batch_size=100)
+
+    trainer = Trainer(network=net, data_stream=ds, learning_rate=0.01)
+    trainer.train(epoch_count=200)
 
 
 if __name__ == "__main__":
-	main()
+    main()
 
